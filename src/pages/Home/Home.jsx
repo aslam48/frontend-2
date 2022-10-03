027df011ae4ccdf2b5c1307369be338704cc6fa7
Belloshehu <belloshehu1@gmail.com>
2022-10-03 21:09:06 +0100

    Add section to state  user status
    resolve #4

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Hero from '../../components/Hero/Hero'
+import StatusList from '../../components/StatusList/StatusList'
 
 const Home = () => {
   return (
     <div>
         <Header />
         <Hero />
+        <StatusList />
     </div>
   )
 }
