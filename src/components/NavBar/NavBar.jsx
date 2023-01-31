9c61147399885f98f4cbc3bc62ba4e79d149d78b
Belloshehu <belloshehu1@gmail.com>
2023-01-31 01:53:50 +0100

    Add Navbar component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <nav className='flex gap-4'>
+        <Link 
+            to='/services' 
+            className='hidden lg:flex no-underline p-2 px-7 text-black rounded-full hover:bg-slate-200'
+        >
+            Services
+        </Link>
+        <Link 
+            to='/login' 
+            className='no-underline py-1 md:py-2 px-7 bg-primary text-white rounded-full hover:bg-black hover:text-primary'
+        >
+            Login
+        </Link>
+    </nav>
+  )
+}
+
+export default NavBar
